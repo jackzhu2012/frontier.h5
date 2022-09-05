ee8f21251c565eb1434f4d11062dbfca80336135
jack <jackzhu98@yahoo.com>
2022-09-05 11:32:26 +0800

    mock

---
+++ mock/mockdb/role.ts
@@ -0,0 +1,36 @@
+import { RoleBean } from "routes/type";
+import { asyncRoutes, staticRoutes } from './routes'
+
+export const routes = [...staticRoutes, ...asyncRoutes]
+
+const roles: RoleBean[] = [
+  {
+    key: 'admin',
+    name: 'admin',
+    description: 'Super Administrator. Have access to view all pages.',
+    routes: routes
+  },
+  {
+    key: 'editor',
+    name: 'editor',
+    description: 'Normal Editor. Can see all pages except permission page',
+    routes: routes.filter(i => i.path !== '/permission') // Just a mock
+  },
+  {
+    key: 'visitor',
+    name: 'visitor',
+    description: 'Just a visitor. Can only see the home page and the document page',
+    routes: [{
+      path: '',
+      redirect: 'dashboard',
+      children: [
+        {
+          path: 'dashboard',
+          name: 'Dashboard',
+          meta: { title: 'dashboard', icon: 'dashboard' }
+        }
+      ]
+    }]
+  }
+]
+export default roles
