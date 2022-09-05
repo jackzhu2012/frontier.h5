d6ca46ee11167b5215128e4f76c4034f9c7b3604
jack <jackzhu98@yahoo.com>
2022-09-05 14:48:28 +0800

    修复windows下不能运行的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path-browserify'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -11,8 +11,7 @@ export default ({ mode }) => {
     ],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, "./src"),
-        path: "path-browserify"
+        '@': path.resolve(__dirname, "src")
       }
     },
     css: {
